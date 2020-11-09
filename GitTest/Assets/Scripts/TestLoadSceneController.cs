9a8f4bd3d5aef45b56d023f04af944d1c768f1de
MahiroNaito <mahirosubofficial@gmail.com>
2020-11-09 22:09:01 +0900

    StartUp Update
    最初の更新やでー

---
+++ GitTest/Assets/Scripts/TestLoadSceneController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TestLoadSceneController : MonoBehaviour
+{
+    public void Push_Return()
+    {
+        SceneManager.LoadScene("Title");
+    }
+}
---
+++ GitTest/Assets/Scripts/TitleController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleController : MonoBehaviour
+{
+    //init var
+    GameObject selectAspect,selectScene,buttonStart;
+    bool setAspect;
+    int setScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //ゲームオブジェクトの確保
+        selectAspect = GameObject.Find("Select_Aspect");
+        selectScene = GameObject.Find("Select_Scene");
+        buttonStart = GameObject.Find("Button_Start");
+
+        //アスペクト選択の初期化(true:portrait,falese:landscape)
+        setAspect = true;
+        //Scene選択の初期化(0~4:Scene1~4に対応)
+        setScene = 0;
+        
+    }
+
+    public void Push_Start()
+    {
+        SceneManager.LoadScene("TestLoadScene");
+    }
+}
